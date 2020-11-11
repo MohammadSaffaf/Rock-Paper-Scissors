b26f9be45d3e3a35f2becffe8dcbaeb3bc98aeaa
MohammadSaffaf <mhmdsaffha@gmail.com>
2020-11-11 18:04:17 +0100

    CSS 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,9 @@ let roundResult=document.getElementById('rounds-result')
 let roundMin=document.getElementById('rounds-min')
 let roundsMax=document.getElementById('rounds-max')
 let lett=document.getElementById('let')
+let section=document.querySelector('body')
+let bilder=document.getElementById("bilder")
+
 let mUser = 0;
 let mComp = 0;
 let mall =0
@@ -40,9 +43,12 @@ valuee=0
     
     scissors.addEventListener("click", () => {
         
-        // rockSpan.style.borderColor = "#fff";
-        // paperSpan.style.borderColor = "#fff";
-        // scissorsSpan.style.borderColor = "#fff";
+        // rock.style.borderColor = "rgb(255, 255, 255)";
+        // rock.style.backgroundColor = "rgb(255, 255, 255)";
+        // paper.style.borderColor = "#rgb(255, 255, 255)";
+        // paper.style.backgroundColor = "#rgb(255, 255, 255)";
+        // // scissors.style.borderColor = "#rgb(255, 255, 255)";
+        // // scissors.style.backgroundColor = "#rgb(255, 255, 255)";
         // let round = getRounds();
         // container.style.display = "none";
         // roundContainer.style.display = "block";
@@ -55,25 +61,39 @@ valuee=0
         } else if (mall <= manyRunde()) {
             roundMin.innerHTML = mall++;
           console.log("finish");
+          show.style.display= "none"
+          roundResult.style.display= "none"
+          bilder.style.display="none"
         //   test.style.display = "none";
         //   rechnen();
+        endResult()
         }
         if (x == 0) {
           user.innerHTML = `${++mUser}`;
           comp.innerHTML = `${mComp * 1}`;
         //   scissorsSpan.style.borderColor = "green";
-          lett.innerHTML = `<p><b>Scissors</b> VS <b>Paper</b>. <span style="color:green">You win!</span></p>`;
+        //   lett.innerHTML = `<p><b>Scissors</b> VS <b>Paper</b>. <span style="color:green">You win!</span></p>`;
+        lett.innerHTML = `<p> SCISSORES<span>[user]</span> PAPER <span>[comp]</span> YOU WIN</p> `;
+        // scissors.style.borderColor="yellow"
+        // scissors.style.backgroundColor="rgba(0, 150, 0, 0.5)"
         } else if (x == 1) {
           user.innerHTML = `${mUser * 1}`;
           comp.innerHTML = `${++mComp}`;
         //   scissorsSpan.style.borderColor = "red";
-          lett.innerHTML = `<p><b>Scissors</b> VS <b>Rock</b>. <span style="color:red">You lose!</span></p>`;
+        //   lett.innerHTML = `<p><b>Scissors</b> VS <b>Rock</b>. <span style="color:red">You lose!</span></p>`;
+        lett.innerHTML = `<p> SCISSORES<span>[user]</span> ROCK <span>[comp]</span> YOU LOSE</p> `;
+        // scissors.style.borderColor="black"
+        // scissors.style.backgroundColor="rgba(255, 0, 0, 0.5)"
         } else {
           user.innerHTML = `${++mUser}`;
           comp.innerHTML = `${++mComp}`;
         //   scissorsSpan.style.borderColor = "grey";
-          lett.innerHTML = `<p>It was a <span style="color:grey">Draw</span>. You both chose <b>Scissors</b>.</p>`;
+        //   lett.innerHTML = `<p>It was a <span style="color:grey">Draw</span>. You both chose <b>Scissors</b>.</p>`;
+        lett.innerHTML = `<p>It was a DRAW. You both chose SCISSORES.</p>`
+        // scissors.style.borderColor="green"
+        //  scissors.style.backgroundColor="rgba(0, 0, 200, 0.7)"
          }
+         
        });
 
 
@@ -82,9 +102,12 @@ valuee=0
 
        rock.addEventListener("click", () => {
         // console.log(mall.target);
-        // rockSpan.style.borderColor = "#fff";
-        // paperSpan.style.borderColor = "#fff";
-        // scissorsSpan.style.borderColor = "#fff";
+        // // rock.style.borderColor = "rgb(255, 255, 255)";
+        // // rock.style.backgroundColor = "rgb(255, 255, 255)";
+        // paper.style.borderColor = "#rgb(255, 255, 255)";
+        // paper.style.backgroundColor = "#rgb(255, 255, 255)";
+        // scissors.style.borderColor = "#rgb(255, 255, 255)";
+        // scissors.style.backgroundColor = "#rgb(255, 255, 255)";
         // let round = getRounds();
         // container.style.display = "none";
         // roundContainer.style.display = "block";
@@ -96,8 +119,10 @@ valuee=0
           roundMin.innerHTML = mall;
         } else if (mall <= manyRunde()) {
           roundMin.innerHTML = mall++;
-          console.log("finish");
-          test.style.display = "none";
+        //   console.log("finish");
+          show.style.display= "none"
+          roundResult.style.display= "none"
+          bilder.style.display="none"
         //   rechnen();
         endResult()
         
@@ -107,25 +132,37 @@ valuee=0
           user.innerHTML = `${mUser * 1}`;
           comp.innerHTML = `${++mComp}`;
         //   rockSpan.style.borderColor = "red";
-          lett.innerHTML = `<p><b>Rock</b> VS <b>Paper</b> . <span style="color:red">You lose!</span></p>`;
+        //   lett.innerHTML = `<p><b>Rock</b> VS <b>Paper</b> . <span style="color:red">You lose!</span></p>`;
+        lett.innerHTML = `<p> ROCK<span>[user]</span> PAPER <span>[comp]</span> YOU LOSE</p> `;
+        // rock.style.borderColor="black"
+        // rock.style.backgroundColor="rgba(255, 0, 0, 0.5)"
         } else if (x == 1) {
           user.innerHTML = `${++mUser}`;
           comp.innerHTML = `${++mComp}`;
         //   rockSpan.style.borderColor = "grey";
-          lett.innerHTML = `<p>It was a <span style="color:grey">Draw</span>. You both chose <b>Rock</b>.</p>`;
+        //   lett.innerHTML = `<p>It was a <span style="color:grey">Draw</span>. You both chose <b>Rock</b>.</p>`;
+        lett.innerHTML = `<p>It was a DRAW. You both chose ROCK.</p>`
+        // rock.style.borderColor="green"
+        // rock.style.backgroundColor="rgba(0, 0, 200, 0.7)"
         } else {
           user.innerHTML = `${++mUser}`;
           comp.innerHTML = `${mComp * 1}`;
         //   rockSpan.style.borderColor = "green";
-          lett.innerHTML = `<p><b>Rock</b> VS <b>Scissors</b>. <span style="color:green">You win!</span></p>`;
+        //   lett.innerHTML = `<p> ROCK <span>[user]</span> SCISSORS <span>[comp]</span> YOU WIN</p>`;
+        lett.innerHTML = `<p> ROCK<span>[user]</span> SCISSORS <span>[comp]</span> YOU WIN</p> `;
+        // rock.style.borderColor="yellow"
+        // rock.style.backgroundColor="rgba(0, 150, 0, 0.5)"
         }
       });
   
       paper.addEventListener("click", () => {
         // console.log(mall.target);
-        // rockSpan.style.borderColor = "#fff";
-        // paperSpan.style.borderColor = "#fff";
-        // scissorsSpan.style.borderColor = "#fff";
+        // rock.style.borderColor = "rgb(255, 255, 255)";
+        // rock.style.backgroundColor = "rgb(255, 255, 255)";
+        // // paper.style.borderColor = "#rgb(255, 255, 255)";
+        // // paper.style.backgroundColor = "#rgb(255, 255, 255)";
+        // scissors.style.borderColor = "#rgb(255, 255, 255)";
+        // scissors.style.backgroundColor = "#rgb(255, 255, 255)";
         // let round = getRounds();
         // container.style.display = "none";
         // roundContainer.style.display = "block";
@@ -137,7 +174,10 @@ valuee=0
           roundMin.innerHTML = mall;
         } else if (mall <= manyRunde()) {
           roundMin.innerHTML = mall++;
-          console.log("finish");
+        //   console.log("finish");
+          show.style.display= "none"
+          roundResult.style.display= "none"
+          bilder.style.display="none"
         //   test.style.display = "none";
         //   rechnen();
         endResult()
@@ -148,29 +188,47 @@ valuee=0
           comp.innerHTML = `${++mComp}`;
         //   rockSpan.style.borderColor = "red";
         //   lett.innerHTML = `<p><b>Rock</b> VS <b>Paper</b> . <span style="color:red">You lose!</span></p>`;
-        lett.innerHTML = `<p> ROCK <span>[user]</span> PAPER <span>[comp]</span></p> `
+        lett.innerHTML = `<p>It was a DRAW. You both chose PAPER.</p>`
+        // paper.style.borderColor="green"
+        // paper.style.backgroundColor="rgba(0, 0, 200, 0.7)"
         } else if (x == 1) {
           user.innerHTML = `${++mUser}`;
           comp.innerHTML = `${++mComp}`;
         //   rockSpan.style.borderColor = "grey";
-          lett.innerHTML = `<p>It was a <span style="color:grey">Draw</span>. You both chose <b>Rock</b>.</p>`;
+          lett.innerHTML = `<p> PAPER<span>[user]</span> ROCK <span>[comp]</span> YOU WIN</p> `;
+        //   paper.style.borderColor="yellow"
+        //   paper.style.backgroundColor="rgba(0, 150, 0, 0.5)"
         } else  {
           user.innerHTML = `${++mUser}`;
           comp.innerHTML = `${mComp * 1}`;
         //   rockSpan.style.borderColor = "green";
-          lett.innerHTML = `<p><b>Rock</b> VS <b>Scissors</b>. <span style="color:green">You win!</span></p>`;
-        }
+        //   lett.innerHTML = `<p><b>Rock</b> VS <b>Scissors</b>. <span style="color:green">You win!</span></p>`;
+        lett.innerHTML = `<p> PAPER <span>[user]</span> SCISSORS <span>[comp]</span> YOU LOSE</p>` 
+        // paper.style.borderColor="black"
+        // paper.style.backgroundColor="rgba(255, 0, 0, 0.5)"
+        } 
       });
 
       function endResult() {
         if (user.innerHTML > comp.innerHTML) {
-          lett.innerHTML = "Congratulations! You win! 🥳<br>Play again";
+            section.style.backgroundImage="url('https://media2.giphy.com/media/jof1Ck9hcZIOlQyzDL/source.gif')"
+            
+          lett.innerHTML = '<p style="font-size: 60px; color: yellow; font-family: Cabin Sketch, cursive;">Congratulations! You win!</p>'
         } else if (user.innerHTML < comp.innerHTML) {
-          lett.innerHTML = "You lose. 🥺 Try again";
+            section.style.backgroundImage="url('https://i.pinimg.com/originals/93/94/01/939401aab667fb28f7d9aaa5c8dc1a3a.gif')"
+            section.style.backgroundRepeat="no-repeat"
+            section.style.backgroundPosition="center"
+            
+          lett.innerHTML = '<p style="font-size: 60px; color: yellow; font-family: Cabin Sketch, cursive;">SOORY! You LOS!</p>';
         } else if ((user.innerHTML = comp.innerHTML)) {
-          lett.innerHTML = "It is a draw. 🤝<br>Try to win";
+            section.style.backgroundImage="url(https://media1.giphy.com/media/LqrvwdcH813d7vgUqZ/giphy.gif) "
+            section.style.backgroundRepeat="no-repeat"
+            section.style.backgroundPosition="center"
+            lett.innerHTML = '<p style="font-size: 60px; color: yellow; font-family: Cabin Sketch, cursive;">IT IS DRAW! PLAY AGIN!</p>'
+        //   lett.innerHTML = "It is a draw. 🤝<br>Try to win";
         }
-        // hier.style.display = "block";
+        style.display = "block";
+        
       }
       
     //   function restarten() {
